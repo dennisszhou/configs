61c8b90acd49a551ae7e1aebcccd3c78f4864501
dennisz <dennisz@node.dennisz-vm1.openlambda-v2-pg0.utah.cloudlab.us>
2017-09-20 03:05:42 +0000

    automate configs

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# bashrc
+if [[ $1 = "mac" ]]; then
+  cp mac/bashrc ~/.bashrc
+else
+  cp linux/bashrc ~/.bashrc
+fi
+echo -ne "\n" >> ~/.bashrc
+cat common/bashrc >> ~/.bashrc
+cat common/bash_profile >> ~/.bash_profile
+
+# tmux
+install_dir="$HOME/.tmux/plugins/tpm"
+if [ ! -d $install_dir ]; then
+  git clone https://github.com/tmux-plugins/tpm $install_dir
+fi
+cp common/tmux ~/.tmux.conf
+
+
+# vim
+install_dir="$HOME/.vim/bundle/Vundle.vim"
+if [ ! -d $install_dir ]; then
+echo $install_dir
+  git clone https://github.com/VundleVim/Vundle.vim.git $install_dir
+fi
+cp common/vimrc ~/.vimrc
+vim +PluginInstall +qall
