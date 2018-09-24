d727cf89a54d58f923c664c081a091607f883369
Dennis Zhou <dennis@kernel.org>
2018-09-24 14:03:41 -0700

    update configure script to do install only

--- configure.sh
+++ configure.sh
@@ -1,44 +1,69 @@
 #/bin/bash
 
-# bashrc
-if [[ -e $HOME/.bashrc ]]; then
-    cp $HOME/.bashrc $HOME/.bashrc.old
-fi
-if [[ $1 = "mac" ]]; then
-  cp mac/bashrc $HOME/.bashrc
-else
-  cp linux/bashrc $HOME/.bashrc
-fi
-echo -ne "\n" >> $HOME/.bashrc
-cat common/bashrc >> $HOME/.bashrc
-cat common/bash_profile >> $HOME/.bash_profile
+PLATFORM="linux"
+INSTALL_ONLY=false
 
-# tmux
-if [[ -e $HOME/.tmux.conf ]]; then
-    cp $HOME/.tmux.conf $HOME/.tmux.conf.old
-fi
-install_dir="$HOME/.tmux/plugins/tpm"
-if [ ! -d $install_dir ]; then
-  git clone https://github.com/tmux-plugins/tpm $install_dir
-fi
-cp common/tmux $HOME/.tmux.conf
-$HOME/.tmux/plugins/tpm/bin/install_plugins
+setup_configs() {
+	# bashrc
+	if [[ -e $HOME/.bashrc ]]; then
+		cp $HOME/.bashrc $HOME/.bashrc.old
+	fi
+	if [[ $PLATFORM = "mac" ]]; then
+		cp mac/bashrc $HOME/.bashrc
+	else
+		cp linux/bashrc $HOME/.bashrc
+	fi
+	echo -ne "\n" >> $HOME/.bashrc
+	cat common/bashrc >> $HOME/.bashrc
+	cat common/bash_profile >> $HOME/.bash_profile
 
+	# tmux
+	if [[ -e $HOME/.tmux.conf ]]; then
+    	    	cp $HOME/.tmux.conf $HOME/.tmux.conf.old
+	fi
+	cp common/tmux $HOME/.tmux.conf
 
-# vim
-if [[ -e $HOME/.vimrc ]]; then
-    cp $HOME/.vimrc $HOME/.vimrc.old
-fi
-install_dir="$HOME/.vim/bundle/Vundle.vim"
-if [ ! -d $install_dir ]; then
-echo $install_dir
-  git clone https://github.com/VundleVim/Vundle.vim.git $install_dir
-fi
-cp common/vimrc $HOME/.vimrc
-vim +PluginInstall +qall
+	# vim
+	if [[ -e $HOME/.vimrc ]]; then
+    	    	cp $HOME/.vimrc $HOME/.vimrc.old
+	fi
+	cp common/vimrc $HOME/.vimrc
 
-# git
-if [[ -e $HOME/.gitconfig ]]; then
-    cp $HOME/.gitconfig $HOME/.gitconfig.old
+	# git
+	if [[ -e $HOME/.gitconfig ]]; then
+    	    	cp $HOME/.gitconfig $HOME/.gitconfig.old
+	fi
+	cp common/gitconfig $HOME/.gitconfig
+}
+
+install_plugins() {
+	# tmux tpm
+	install_dir="$HOME/.tmux/plugins/tpm"
+	if [ ! -d $install_dir ]; then
+  	  	git clone https://github.com/tmux-plugins/tpm $install_dir
+	fi
+	$HOME/.tmux/plugins/tpm/bin/install_plugins
+
+	# vim
+	install_dir="$HOME/.vim/bundle/Vundle.vim"
+	if [ ! -d $install_dir ]; then
+		echo $install_dir
+  	  	git clone https://github.com/VundleVim/Vundle.vim.git $install_dir
+	fi
+	vim +PluginInstall +qall
+}
+
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+		-p) PLATFORM="$2"; shift 2;;
+		--platform=*) PLATFORM="${1#*=}"; shift 1;;
+		--install-only) INSTALL_ONLY=true; shift 1;;
+	esac
+done
+
+# should I configure?
+if [ "$INSTALL_ONLY" = false ]; then
+	setup_configs
 fi
-cp common/gitconfig $HOME/.gitconfig
+
+install_plugins
