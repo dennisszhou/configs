44fb97aa524f018ea4ebfb6a341c2a4440efdec5
Dennis Zhou <dennis@kernel.org>
2026-03-01 20:31:06 -0800

    rc: symlink to git repo

--- configure.sh
+++ configure.sh
@@ -2,39 +2,42 @@
 
 # --- Helper Functions ---
 
-# Backs up a file to .old if it exists, then copies the source to destination.
+# Symlinks a file, backing up the original if it exists and isn't already a link.
 install_file() {
-    local src="$1"
+    local src="$(pwd)/$1"
     local dest="$2"
-    if [[ -e "$dest" ]]; then
+
+    if [[ -L "$dest" ]]; then
+        rm "$dest"
+    elif [[ -e "$dest" ]]; then
         echo "Backing up $dest to $dest.old"
-        cp "$dest" "$dest.old"
+        mv "$dest" "$dest.old"
     fi
-    cp "$src" "$dest"
+
+    echo "Linking $dest -> $src"
+    ln -s "$src" "$dest"
 }
 
-# Deploys modular shell components to ~/.shell_config and sets up shell entry points.
+# Sets up shell entry points by linking directly to the repo files.
 setup_shell_config() {
     local platform="$1"
-    local CONFIG_HOME="$HOME/.shell_config"
-    mkdir -p "$CONFIG_HOME/os"
-    mkdir -p "$CONFIG_HOME/functions"
 
-    echo "Deploying modular shell components to $CONFIG_HOME..."
-    install_file "common/shrc"    "$CONFIG_HOME/shrc"
-    install_file "common/aliases" "$CONFIG_HOME/aliases"
-    cp common/functions/* "$CONFIG_HOME/functions/"
+    echo "Setting up shell entry points (linking directly to repo)..."
 
     if [[ "$platform" == "mac" ]]; then
-        install_file "os/mac"       "$CONFIG_HOME/os/mac"
-        install_file "zsh/zshrc"    "$HOME/.zshrc"
         install_file "zsh/zprofile" "$HOME/.zprofile"
+        install_file "zsh/zshrc"    "$HOME/.zshrc"
         [[ -e "$HOME/.profile" ]] && mv "$HOME/.profile" "$HOME/.profile.old"
     else
-        install_file "os/linux"          "$CONFIG_HOME/os/linux"
-        install_file "bash/bashrc"       "$HOME/.bashrc"
         install_file "bash/bash_profile" "$HOME/.bash_profile"
+        install_file "bash/bashrc"       "$HOME/.bashrc"
         [[ -e "$HOME/.profile" ]] && mv "$HOME/.profile" "$HOME/.profile.old"
+    fi
+
+    # Clean up old modular directory if it exists
+    if [[ -d "$HOME/.shell_config" ]]; then
+        echo "Removing old ~/.shell_config directory..."
+        rm -rf "$HOME/.shell_config"
     fi
 }
 
@@ -48,7 +51,7 @@ setup_other_configs() {
     # Vim
     install_file "common/vimrc" "$HOME/.vimrc"
     mkdir -p "$HOME/.vim/ftplugin"
-    cp common/vim/c.vim "$HOME/.vim/ftplugin/"
+    install_file "common/vim/c.vim" "$HOME/.vim/ftplugin/c.vim"
     mkdir -p "$HOME/.vim/undodir"
 
     # Git
